e86325ea8f10cafdd80ec0c1cd8bb809f0e0a3e9
valeriy korovin <1k9o9r5@gmail.com>
2024-02-23 00:07:27 +0200

    HW5_1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+// 1й вариант
+// let nameUser = prompt('Hello! What is your name?');
+// alert("Hello," + " " + nameUser + "!" + " " + "How are you?")
+
+// 2й вариант
+// let nameUser = prompt('Hello! What is your name?');
+// let answer = "Hello," + " " + nameUser + "!" + " " + "How are you?"
+// alert(answer)
+
+
+// 3й вариант
+let nameUser = prompt('Hello! What is your name?');
+let answer = `Hello, ${nameUser}! How are you?`
+alert(answer)
