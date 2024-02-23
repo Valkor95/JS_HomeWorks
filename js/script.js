e26a879d9b59f42b0d3050eb15340060cb7a362d
valeriy korovin <1k9o9r5@gmail.com>
2024-02-23 02:46:07 +0200

    HW6_1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+let user1 = prompt();
+let user2 = prompt();
+let user3 = prompt();
+alert(user1+user2+user3);
+
+
+let number = 12345;
+let string = String(number);
+// let result = string[0]+ " " + string[1] + " " + string[2]+ " " + string[3] + " " + string[4]
+let result = `${string[0]} ${string[1]} ${string[2]} ${string[3]} ${string[4]}`
+console.log(result)
+
+
+
