242a773d3e52260211d671f8acd98719f1b0e276
valeriy korovin <1k9o9r5@gmail.com>
2024-02-26 19:07:38 +0200

    HW6_2_FIX

--- js/script.js
+++ js/script.js
@@ -6,10 +6,10 @@ let user3 = prompt();
 alert(user1+user2+user3);
 
 
-let number = 12345;
-let string = String(number);
+const number = 12345;
+const string = String(number);
 // let result = string[0]+ " " + string[1] + " " + string[2]+ " " + string[3] + " " + string[4]
-let result = `${string[0]} ${string[1]} ${string[2]} ${string[3]} ${string[4]}`
+const result = `${string[0]} ${string[1]} ${string[2]} ${string[3]} ${string[4]}`
 console.log(result)
 
 
