a7e4be201a405e626c7d76250a013778e8d0986d
valeriy korovin <1k9o9r5@gmail.com>
2024-02-28 23:50:10 +0200

    HW8_2_FIX

--- js/script.js
+++ js/script.js
@@ -1,21 +1,15 @@
 'use strict';
 
+
 const age = prompt('Please, enter your age!');
-
-
-switch (true){
-    case age > 0:
-        alert(`${(+age+10)} - your age after 10 years!`)
-        break;
-    case age !== null:
-        alert(`Error! Do you want repeat again?`)
-        break;
-    case age != 0:
-        alert(`Do you want repeat again?`)
-        break;
-    default:
-        confirm('Something is wrong! Do you want repeat again?')
+if (age >= 0) {
+    const futAge = +age + 10;
+    alert(futAge + ' - this is your age after 10 years!')
+} else {
+    alert('Error! Try again!')
 }
+
+
 
 
 
