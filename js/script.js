6a1df8a78e9437f483efa4ced38f88002a6c5568
valeriy korovin <1k9o9r5@gmail.com>
2024-02-28 15:17:57 +0200

    HW9

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 'use strict';
 
+const celsToFar = c => {
+    return `${((c*9)/5)+32} °F`;
+}
+alert(celsToFar(1))
+console.log(celsToFar(1))
+
+const farToCels = f => {
+    return `${((f-32)*5)/9} ℃`;
+}
+alert(farToCels(1))
+console.log(farToCels(1))
