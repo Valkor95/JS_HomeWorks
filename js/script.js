d40276c6b80d316206de012104bc76150e8e75a3
valeriy korovin <1k9o9r5@gmail.com>
2024-02-28 14:48:30 +0200

    HW8

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 'use strict';
 
+let age = prompt('Please, enter your age!');
+
+
+switch (true){
+    case age > 0:
+        alert(`${(+age+10)} - your age after 10 years!`)
+        break;
+    case age !== null:
+        alert(`Error! Do you want repeat again?`)
+        break;
+    case age != 0:
+        alert(`Do you want repeat again?`)
+        break;
+    default:
+        confirm('Something is wrong! Do you want repeat again?')
+}
+
+
+
