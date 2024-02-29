f2eba39b4c0aadf17f82e10276fe1b5e4bb5493c
valeriy korovin <1k9o9r5@gmail.com>
2024-02-29 22:30:37 +0200

    HW12

--- js/script.js
+++ js/script.js
@@ -1,2 +1,38 @@
 'use strict';
 
+// Task 1
+const user = {};
+
+user.name = 'John';
+user.surname = 'Smith';
+
+user.name = 'Pete';
+delete user.name;
+console.log(user);
+
+//Task 2
+
+const user1 = {
+    name: 'John'
+};
+
+user1.name = 'Pete';
+console.log(user1);
+
+/* Изменения произойдут. Несмотря на то, что используется const,
+внутри объекта можно производить изменения свойств и их значений.
+Const делает неизменным сам object, но не его содержимое, т.е. так как
+object это ссылочный тип данных, то ссылка на объект не будет изменена.
+*/
+
+// Task 3
+
+const salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+const sum = salaries.John + salaries['Ann'] + salaries.Pete;
+
+console.log(sum)
