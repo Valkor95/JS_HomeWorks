de7932b4d9ca13ea7732fa9341434d07b9833ae4
valeriy korovin <1k9o9r5@gmail.com>
2024-03-02 23:51:32 +0200

    HW11_2_FIX

--- js/script.js
+++ js/script.js
@@ -1,25 +1,20 @@
 'use strict';
 
+
 const request = prompt('What do you want to do? \n Example: add, sub, mult, div');
+const value1 = parseInt(prompt('Add value #1'));
+const value2 = parseInt(prompt('Add value #2'));
 if (request === 'add') {
-    const value = parseInt(prompt('Add value #1'));
-    const value2 = parseInt(prompt('Add value #2'));
-    const sum1 = value + value2;
+    const sum1 = value1 + value2;
     alert(sum1)
 } else if (request === 'sub') {
-    const value3 = parseInt(prompt('Add value #1'));
-    const value4 = parseInt(prompt('Add value #2'));
-    const sum2 = value3 - value4;
+    const sum2 = value1 - value2;
     alert(sum2)
 } else if (request === 'mult') {
-    const value5 = parseInt(prompt('Add value #1'));
-    const value6 = parseInt(prompt('Add value #2'));
-    const sum3 = value5 * value6;
+    const sum3 = value1 * value2;
     alert(sum3)
 } else if (request === 'div') {
-    const value7 = parseInt(prompt('Add value #1'));
-    const value8 = parseInt(prompt('Add value #2'));
-    const sum4 = value7 / value8;
+    const sum4 = value1 / value2;
     alert(sum4)
 } else {
     alert('Error! Enter only: add, sub, mult, div!')
