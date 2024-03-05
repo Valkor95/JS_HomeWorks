c10afeaefa9016ff3bbc841faebf610139f3559e
valeriy korovin <1k9o9r5@gmail.com>
2024-03-05 18:22:21 +0200

    HW16

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
 'use strict';
 
+const value1 = parseInt(prompt('Enter value #1'));
+const value2 = parseInt(prompt('Enter value #2'));
+const value3 = parseInt(prompt('Enter value #3'));
+
+const average = (value1 + value2 + value3)/3 ;
+alert(`This is your average - ${average}`)
