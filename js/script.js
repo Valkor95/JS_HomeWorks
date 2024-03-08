fe032411fe070556d86a107edaebaf37c3d748a9
valeriy korovin <1k9o9r5@gmail.com>
2024-03-08 21:36:38 +0200

    HW21_1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,111 @@
 'use strict';
 
+// Task 1
+// const a = +prompt('Значение');
+// const solution = a === 0 ? 'Верно' : 'Неправильно';
+//
+// console.log(solution);
+// alert(solution);
+
+// Task 2
+// const a = +prompt('Значение');
+// const solution = a > 0 ? 'Верно' : 'Неправильно';
+//
+// console.log(solution);
+// alert(solution);
+
+// Task 3
+// const a = +prompt('Значение');
+// const solution = a < 0 ? 'Верно' : 'Неправильно';
+//
+// console.log(solution);
+// alert(solution);
+
+// Task 4
+// const a = +prompt('Значение');
+// const solution = a >= 0 ? 'Верно' : 'Неправильно';
+//
+// console.log(solution);
+// alert(solution);
+
+// Task 5
+// const a = +prompt('Значение');
+// const solution = a <= 0 ? 'Верно' : 'Неправильно';
+//
+// console.log(solution);
+// alert(solution);
+
+// Task 6
+// const a = +prompt('Значение');
+// const solution = a !== 0 ? 'Верно' : 'Неправильно';
+//
+// console.log(solution);
+// alert(solution);
+
+// Task 7
+// const a = prompt('Значение');
+// const solution = a === 'test' ? 'Верно' : 'Неправильно';
+//
+// console.log(solution);
+// alert(solution);
+
+// Task 8
+// const a = +prompt('Значение');
+// const solution = a === 1 ? 'Верно' : 'Неправильно';
+//
+// console.log(solution);
+// alert(solution);
+
+// Task 9
+// const a = +prompt('Значение');
+// const solution = a > 0 && a < 5 ? 'Верно' : 'Неправильно';
+//
+// console.log(solution);
+// alert(solution);
+
+// Task 10
+// const a = +prompt('Значение');
+// const solution = a === 0 || a === 2 ? `${a+7}` : `${a/10}`;
+//
+// console.log(solution);
+// alert(solution);
+
+// Task 11
+// const a = +prompt('Значение #1');
+// const b = +prompt('Значение #2');
+// const solution = a <= 1 || (b >= 6 && b < 14) ? `Верно` : `Неверно`;
+//
+// console.log(solution);
+// alert(solution);
+
+// Task 12
+// const a = +prompt('Значение #1');
+// const b = +prompt('Значение #2');
+// const solution = (a > 2 && a < 11) || (b >= 6 && b < 14) ? `Верно` : `Неверно`;
+//
+// console.log(solution);
+// alert(solution);
+
+// Task 13
+const num = +prompt('Введите одно значение: 1, 2, 3 или 4!');
+let result = null;
+
+switch (num) {
+    case 1:
+        result = 'Зима';
+        break;
+    case 2:
+        result = 'Весна';
+        break;
+    case 3:
+        result = 'Лето';
+        break;
+    case 4:
+        result = 'Осень';
+        break;
+    default:
+        alert('Вы ввели неправильное значение!')
+}
+
+console.log(result);
+alert(result);
