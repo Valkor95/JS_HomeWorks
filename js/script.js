f4cef5bce51445e8cd00c946a680ddae8fbae135
valeriy korovin <1k9o9r5@gmail.com>
2024-03-08 02:22:38 +0200

    HW17_1

--- js/script.js
+++ js/script.js
@@ -1,26 +1,23 @@
 'use strict';
 
 let age = prompt('Введіть ваш рік народження!');
-if (age === null) {
+ if (age === null || age === ''){
     age = "Шкода, що Ви не захотіли ввести cвою дату народження!";
-};
+}
 
-const city = prompt('В якому місті ви живете?');
+let city = prompt('В якому місті ви живете?');
 // if (city === null) {
 //     city = "Шкода, що Ви не захотіли ввести cвоє місто";
 // };
 
-let sport = prompt('Який ваш улюблений вид спорту?');
-if (sport === null){
-    sport = "Шкода, що Ви не захотіли ввести cвій улюблений вид спорту!";
-};
+let sport = prompt('Який ваш улюблений вид спорту: джиу-джитсу, бокс чи футбол?');
+
 
 
 let capitalOrTown = null;
-    if (city === null) {
+    if (city === null || city === '') {
         capitalOrTown = "Шкода, що Ви не захотіли ввести cвоє місто!";
-    }
-    else if (city.toLowerCase() === 'київ' || city.toLowerCase() === 'лондон'
+    }  else if (city.toLowerCase() === 'київ' || city.toLowerCase() === 'лондон'
     || city.toLowerCase() === 'вашингтон') {
         capitalOrTown = `Ти живеш у столиці ${city}!`
     } else { capitalOrTown = `Ти живеш у місті ${city}!`}
@@ -29,4 +26,24 @@ let capitalOrTown = null;
 
 
 
-alert(`Рік: ${age} \n Місто: ${capitalOrTown} \n Спорт: ${sport}`);
+let sportStars = {
+        ['джиу-джитсу']: 'Гаррі Тонон',
+        ['бокс']: 'Майк Тайсон',
+        ['футбол']: 'Леон Мессі',
+
+}
+    if (sport === null || sport === ''){
+        sport = "Шкода, що Ви не захотіли ввести cвій улюблений вид спорту!";
+    } else if ( sport.toLowerCase() === 'джиу-джитсу') {
+        sport = `Круто! Хочеш стати як ${sportStars['джиу-джитсу']}?`
+    } else if (sport.toLowerCase() === 'бокс'){
+        sport = `Круто! Хочеш стати як ${sportStars['бокс']}?`
+    } else if (sport.toLowerCase() === 'футбол'){
+    sport = `Круто! Хочеш стати як ${sportStars['футбол']}?`
+} else {
+        sport = 'Помилка! Треба вибрати одне із трьох!'
+    }
+
+
+
+alert(` ${age} \n ${capitalOrTown} \n ${sport}`);
