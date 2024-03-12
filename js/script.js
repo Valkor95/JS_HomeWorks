b67fa76cebbaa05b93e79161c02d2cf2640cfc01
valeriy korovin <1k9o9r5@gmail.com>
2024-03-12 23:09:19 +0200

    HW22_1

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@
 // let value = '';
 // for (let i = 1; i <= 10; i++){
 //     value += i * 7;
-//     if (i < 15){
+//     if (i < 10){
 //         value += ', '
 //     }
 // }
@@ -87,3 +87,120 @@
 // console.log(divThree);
 
 //Task 9
+// const number = prompt("Введите натуральное число");
+// let div = '';
+// for (let i = 1; i <= number; i++){
+//     if (number % i === 0){
+//         if (div !== ''){
+//             div += ', '
+//         }
+//         div += i;
+//     }
+// }
+// alert(div);
+// console.log(div);
+
+//Task 10
+// const number = prompt("Введите натуральное число");
+// let div = '';
+// let count = 0;
+// for (let i = 1; i <= number; i++){
+//     if (number % i === 0){
+//         if (div !== ''){
+//             div += ', '
+//         }
+//         div += i
+//
+//         if (i % 2 === 0){
+//             count++;
+//         }
+//     }
+// }
+// alert(`Делители: ${div} \nКоличество парных делителей: ${count}`);
+// console.log(`Делители: ${div} \nКоличество парных делителей: ${count}`);
+
+//Task 11
+// const number = prompt("Введите натуральное число");
+// let div = '';
+// let count = 0;
+// let sumCount = 0;
+// for (let i = 1; i <= number; i++){
+//     if (number % i === 0){
+//         if (div !== ''){
+//             div += ', '
+//         }
+//         div += i
+//
+//         if (i % 2 === 0){
+//             sumCount += i;
+//             count++;
+//         }
+//     }
+// }
+// alert(`Делители: ${div} \nКоличество парных делителей: ${count} \nСумма парных делителей ${sumCount}`);
+// console.log(`Делители: ${div} \nКоличество парных делителей: ${count} \nСумма парных делителей ${sumCount}`);
+
+// Task 12
+let value1 = '';
+let value2 = '';
+let value3 = '';
+let value4 = '';
+let value5 = '';
+let value6 = '';
+let value7 = '';
+let value8 = '';
+let value9 = '';
+let value10 = '';
+
+for (let i = 1; i <= 10; i++){
+    value1 += i;
+    if (i < 10){
+        value1 += ', '
+    }
+    value2 += i * 2;
+    if (i < 10){
+        value2 += ', '
+    }
+    value3 += i * 3;
+    if (i < 10){
+        value3 += ', '
+    }
+    value4 += i * 4;
+    if (i < 10){
+        value4 += ', '
+    }
+    value5 += i * 5;
+    if (i < 10){
+        value5 += ', '
+    }
+    value6 += i * 6;
+    if (i < 10){
+        value6 += ', '
+    }
+    value7 += i * 7;
+    if (i < 10){
+        value7 += ', '
+    }
+    value8 += i * 8;
+    if (i < 10){
+        value8 += ', '
+    }
+    value9 += i * 9;
+    if (i < 10){
+        value9 += ', '
+    }
+    value10 += i * 10;
+    if (i < 10){
+        value10 += ', '
+    }
+}
+
+alert(`1: ${value1} \n 2: ${value2} \n 3: ${value3}\n 
+4: ${value4} \n 5: ${value5} \n 6: ${value6}
+\n 7: ${value7} \n 8: ${value8} \n 9: ${value9}
+\n 10: ${value10}`);
+
+console.log(`1: ${value1} \n 2: ${value2} \n 3: ${value3}
+\n 4: ${value4} \n 5: ${value5} \n 6: ${value6}
+\n 7: ${value7} \n 8: ${value8} \n 9: ${value9}
+\n 10: ${value10}`);
