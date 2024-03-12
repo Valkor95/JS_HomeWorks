2a1f6bb036e7160981089cdd4cd34ca169949b80
valeriy korovin <1k9o9r5@gmail.com>
2024-03-13 01:32:27 +0200

    HW23

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
 'use strict';
 
+// Task 1
+// let value = '';
+// for (let i = 20; i <= 30; i += 0.5){
+//     value += i;
+//     if (i < 30){
+//         value += ', '
+//     }
+// }
+//
+// alert(value);
+// console.log(value)
+
+//Task 2
+// const exchange = 27;
+// let sum = 0;
+// for (let d = 10; d <= 100; d += 10){
+//     sum += exchange * d;
+//     if (d < 100){
+//         sum += ' гривен, '
+//     }
+// }
+//
+// alert(sum);
+// console.log(sum)
+
+//Task 3
