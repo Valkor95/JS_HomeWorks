348a9b674310749c6148e7fa26c2d45893982a18
valeriy korovin <1k9o9r5@gmail.com>
2024-03-12 22:23:15 +0200

    HW22

--- js/script.js
+++ js/script.js
@@ -1,2 +1,89 @@
 'use strict';
 
+// Task 1
+// let string = '';
+// for  (let i = 10; i <= 20; i++){
+//     string += i;
+//     if (i < 20){
+//         string += ', '
+//     }
+// }
+//
+// alert(string);
+// console.log(string);
+
+//Task 2
+// let value = '';
+// for (let i = 10; i <=20; i++){
+//     value += i * i;
+//     if (i < 20){
+//         value += ', '
+//     }
+// }
+//
+// alert(value);
+// console.log(value);
+
+//Task 3
+// let value = '';
+// for (let i = 1; i <= 10; i++){
+//     value += i * 7;
+//     if (i < 15){
+//         value += ', '
+//     }
+// }
+// alert(value);
+// console.log(value);
+
+//Task 4
+// let sum = 0;
+// for (let i = 0; i <= 15; i++) {
+//     sum += i;
+// }
+// alert(sum);
+// console.log(sum);
+
+// Task 5
+// let product = 1;
+// for (let i = 15; i <= 35; i++){
+//     product *= i;
+// }
+// alert(product);
+// console.log(product);
+
+//Task 6
+// let average = 0;
+// for (let i = 1; i <= 500; i++){
+//     average += i;
+//     if (i === 500){
+//         average /= i;
+//     }
+// }
+// alert(average);
+// console.log(average);
+
+//Task 7
+// let sumEven = 0;
+// for (let i = 30; i <= 80; i++){
+//     if (i % 2 === 0){
+//         sumEven += i;
+//     };
+// }
+// alert(sumEven)
+// console.log(sumEven)
+
+//Task 8
+// let divThree = '';
+// for (let i = 100; i <= 200; i++){
+//     if (i % 3 === 0){
+//        if (divThree !== ''){
+//            divThree += ', '
+//        }
+//        divThree += i;
+//     }
+//
+// }
+// alert(divThree);
+// console.log(divThree);
+
+//Task 9
