c52eada0ed48f8fa80d4728403c6829a8927e848
valeriy korovin <1k9o9r5@gmail.com>
2024-03-13 15:29:43 +0200

    HW26_1

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@
 const value = 20;
 let sumOdd = 0;
 
-outerLoop:
+
 for(let  i = 1; i <= value; i++){
     if (i % 2 === 0){
-        continue outerLoop;
+        continue ;
     }
     sumOdd += i;
 
