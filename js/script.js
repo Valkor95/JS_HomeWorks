15c249eea318bcd276e3ee3660ba02fc05c0e527
valeriy korovin <1k9o9r5@gmail.com>
2024-03-13 14:10:47 +0200

    HW23_1

--- js/script.js
+++ js/script.js
@@ -26,3 +26,54 @@
 // console.log(sum)
 
 //Task 3
+// const N = 1124;
+// let numbers = '';
+// let squareResult = '';
+//
+// for(let i = 1; i <= 100; i++){
+//     let square = i * i;
+//     if (square <= N){
+//         numbers += i + ', ';
+//         squareResult += square + ', ';
+//     }
+// }
+// alert(`Целые числа: ${numbers}\nКвадрат целых чисел: ${squareResult}`);
+// console.log(`Целые числа: ${numbers}\nКвадрат целых чисел: ${squareResult}`)
+
+//Task 4
+// const value = +prompt('Введите число');
+// let result = true;
+//
+// if ( value <= 1){
+//     result = false;
+// } else {
+//     for (let i = 2; i <= Math.sqrt(value); i++){
+//         if (value % i === 0){
+//             result = false;
+//             break;
+//         }
+//     }
+// }
+//
+// if (result){
+//        alert(` ${value} - Это простое число!`);
+//     } else {
+//        alert(` ${value} - Это не простое число!`);
+//     }
+
+//Task 5
+const value = +prompt('Введите число');
+let result = false;
+
+for (let i = 0; Math.pow(3, i) <= value; i++){
+    if (Math.pow(3, i) === value){
+        result = true;
+    }
+}
+
+if (result){
+    alert(` ${value} - можно получить путем возведения 3 в некоторую степень`);
+} else {
+    alert(` ${value} - нельзя получить путем возведения 3 в некоторую степень`);
+}
+
