8a49fe27ba66b3b7819d931dc4b95d7a0a418eda
valeriy korovin <1k9o9r5@gmail.com>
2024-03-13 14:23:59 +0200

    HW24

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
 'use strict';
 
+
+// Task 1
+// const arr = [1, 2, 3, 4, 5];
+// let sum = 0;
+//
+// for (let i = 0; i <= arr.length; i++){
+//     if (Number(arr[i])) {
+//         sum += arr[i];
+//     }
+// }
+//
+// alert(sum);
+// console.log(sum)
+
+// Task 2
+const arr = [1, 2, 3, 4, 5];
+let sum = 0;
+
+for (let i = 0; i <= arr.length; i++){
+    if (Number(arr[i])) {
+        sum += Math.pow(arr[i], 2);
+    }
+}
+
+alert(sum);
+console.log(sum)
