a6af5ebb2d4aab0587e83e3ab937faba7fc062c3
valeriy korovin <1k9o9r5@gmail.com>
2024-03-13 15:28:14 +0200

    HW26

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 'use strict';
 
+const value = 20;
+let sumOdd = 0;
+
+outerLoop:
+for(let  i = 1; i <= value; i++){
+    if (i % 2 === 0){
+        continue outerLoop;
+    }
+    sumOdd += i;
+
+}
+
+alert(`Число ${value}\nСумма непарных чисел ${sumOdd}`)
