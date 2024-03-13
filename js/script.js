430dd5ff0f888a5c318ea0295f7cd90016e3adcb
valeriy korovin <1k9o9r5@gmail.com>
2024-03-13 14:48:05 +0200

    HW25

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 'use strict';
 
+const value = +prompt('Введите число');
+let div = null;
+
+if (value < 1){
+    div = NaN;
+    alert(`Неправильное значение ${div}`)
+} else if (value === 1){
+    div = 1;
+    alert(`Для числа 1 наименьший делитель - ${div}`)
+} else {
+    for (let i = 2; i <= value; i++) {
+        if (value % i === 0) {
+            div = i;
+            break
+        }
+    }
+    alert(`Для числа ${value} наименьший делитель - ${+div}`)
+}
