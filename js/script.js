78029c5c053a04c7f560fecf3e4f2d72e2ac12dd
valeriy korovin <1k9o9r5@gmail.com>
2024-03-15 02:43:56 +0200

    HW28

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 'use strict';
 
+const number = +prompt('Введите число:')
+const degree = +prompt('Введите степень:')
+
+const pow = function (a, b = 1){
+    const result = a ** b;
+    if (isNaN(a) || isNaN(b)){
+        return 'Вы ввели неправильные данные';
+    }
+    return result
+}
+
+const result = pow(number, degree);
+alert(result)
