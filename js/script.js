b9fe7d5a1f112c40e65ec7fdfd8cc20d71dad349
valeriy korovin <1k9o9r5@gmail.com>
2024-03-15 02:25:27 +0200

    HW27

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 'use strict';
 
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
+const generateKey = function (length, characters){
+    let key = '';
+    for (let i = 0; i <= length; i++){
+        const random = Math.floor(Math.random()*characters.length);
+        key += characters[random];
+    }
+    return key;
+}
+
+console.log(generateKey(characters.length, characters))
