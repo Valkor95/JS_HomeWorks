7b34bb169dbd03653ebab33c620263dcf423a8b1
valeriy korovin <1k9o9r5@gmail.com>
2024-03-17 00:14:03 +0200

    HW30

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 'use strict';
 
+const arr = [1, 2, 3, -1, -2, -3];
+
+const functionArr = function (arr){
+    const exampleArr = [];
+    if (arr.length === 0){
+        return null;
+    }
+
+    for (let i = 0; i <= arr.length; i++){
+        if (arr[i] >= 0){
+            exampleArr.push(arr[i])
+        }
+    }
+    return exampleArr;
+}
+
+console.log(functionArr(arr));
+
