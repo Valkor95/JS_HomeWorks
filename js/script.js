325280dbf780996346d0401b8b9cae40facaede2
valeriy korovin <1k9o9r5@gmail.com>
2024-03-25 15:03:41 +0200

    HW33_1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
-'use strict';
+"use strict";
 
+const sum = (a, b) => a + b;
+
+const sub = (a, b) => a - b;
+
+const mult = (a, b) => a * b;
+
+console.log(sum(2, 3));
+console.log(sub(5, 4));
+console.log(mult(6, 7))
