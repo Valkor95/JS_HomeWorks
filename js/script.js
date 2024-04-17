35cfe210e532f62719a747e059f318e52b18fa84
valeriy korovin <1k9o9r5@gmail.com>
2024-04-18 00:20:46 +0300

    HW54_v1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-"use strict";
+"use strict"; //eslint-disable-line
+
+const myBind = function (func, thiS, ...args) {
+  return function (...innerArgs) {
+    if (!args.length) {
+      args = Object.values(thiS);
+    }
+    const allArgs = args.concat(innerArgs);
+    return func(...allArgs);
+  };
+};
+
+const example = function (name, age) {
+  return `My name is ${name} and I am ${age}`;
+};
+
+const obj = {
+  name: 'Liza',
+  year: '33',
+};
+
+const user1 = myBind(example, obj, 'Valentin', '25');
+console.log(user1());
+
+const user2 = myBind(example, obj);
+console.log(user2());
