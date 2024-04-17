018de7e43c6a1767d12745d356d1ab05fd25a2fe
valeriy korovin <1k9o9r5@gmail.com>
2024-04-17 23:21:53 +0300

    HW56_v1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-"use strict";
+'use strict';// eslint-disable-line
+const myApply = function (func, thiS, arr) {
+  if (!arr) {
+    return func(thiS.name);
+  }
+  return func(...arr);
+};
+
+const example = function (name) {
+  return `My name is ${name}`;
+};
+
+const person = {
+  name: 'Valentin',
+};
+
+console.log(myApply(example, person));
+console.log(myApply(example, person, ['Alexander']));
