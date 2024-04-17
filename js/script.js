7770609ec5f1b0f2482652e8ab0e1436006c408f
valeriy korovin <1k9o9r5@gmail.com>
2024-04-18 00:39:20 +0300

    HW55_v1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 "use strict" //eslint-disable-line
+
+const ladder = {
+  step: 0,
+  up() {
+    this.step += 1;
+    return this;
+  },
+  down() {
+    this.step -= 1;
+    return this;
+  },
+  showStep() {
+    alert(this.step);
+    return this;
+  },
+};
+
+ladder.up().up().down().showStep();
