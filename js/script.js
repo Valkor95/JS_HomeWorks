11caa70eee9212015f463e31206d1279f75dc7c1
valeriy korovin <1k9o9r5@gmail.com>
2024-04-24 00:06:41 +0300

    HW57_v1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 "use strict" //eslint-disable-line
+const sumFunc = () => {
+  let n = 0;
+
+  return (num) => {
+    n += num;
+    return n;
+  };
+};
+const sum = sumFunc();
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(12));
+console.log(sum(101));
