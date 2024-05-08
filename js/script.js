260bec96755b975680566d1d47f6b4fbe35cce97
valeriy korovin <1k9o9r5@gmail.com>
2024-05-08 23:26:40 +0300

    HW62_v1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-"use strict" //eslint-disable-line
+'use strict' //eslint-disable-line
+
+(function () {
+  const generateList = function (array) {
+    const ul = document.createElement('ul');
+
+    array.forEach((item) => {
+      const li = document.createElement('li');
+      if (!Array.isArray(item)) {
+        li.textContent = item;
+      } else {
+        const next = generateList(item);
+        li.appendChild(next);
+      }
+
+      ul.appendChild(li);
+    });
+
+    return ul;
+  };
+
+  const array = [1, 2, [1.1, 1.2, 1.3], 3];
+  console.log(generateList(array));
+}());
