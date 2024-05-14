22a6db8f987b97abb8cdfa7bc87cfafe42da5a0f
valeriy korovin <1k9o9r5@gmail.com>
2024-05-14 12:25:02 +0300

    HW63_v3.3_js

--- js/script.js
+++ js/script.js
@@ -25,7 +25,6 @@
                     const li = document.createElement('li');
                     li.textContent = item;
                     category.append(li);
-                    li.addEventListener('click', () => renderProducts(item));
                 })
             }
 
@@ -39,7 +38,41 @@
                 })
             }
 
+            const showInfo = function (product){
+                info.innerHTML = `
+                <h3>${product.name}</h3>;
+                <p>Category: ${product.category}</p>;
+                <p>Price: ${product.price}</p>.
+                <hr>
+                <p>Best choice of your life!</p>
+                <button data-buy-btn>Buy NOW!!!</button>`;
+                document.querySelector('[data-buy-btn]').addEventListener('click', buyProduct);
+            }
+
+            const buyProduct =  () => {
+                alert('Product purchased');
+                renderCategories();
+                product.innerHTML = ''
+            }
+
             renderCategories()
+
+            const eventCategory = (event) => {
+                if (event.target.tagName === 'LI'){
+                    const category = event.target.textContent;
+                    renderProducts(category)
+                }
+            }
+
+            category.addEventListener('click', eventCategory)
+
+            const eventProduct = (event) => {
+                if (event.target.tagName === 'LI'){
+                    const prodName = event.target.textContent.split(' - ')[0];
+                    const product = products.find(p => p.name === prodName);
+                    showInfo(product)
+                }
+            }
         }
 
         document.addEventListener('DOMContentLoaded', eventFunction)
