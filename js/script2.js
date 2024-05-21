e3cd6a17cb56c08dcf3aa4d393b3c9f5f1339b14
valeriy korovin <1k9o9r5@gmail.com>
2024-05-21 23:44:09 +0300

    HW65_v10

--- js/script2.js
+++ js/script2.js
@@ -1,11 +1,12 @@
 (function (){
     const getLS = () =>{
         const formDataGet = localStorage.getItem('formData');
+        const userInfoList = document.querySelector('#userInfo');
 
         if (formDataGet){
             const formDataObj = JSON.parse(formDataGet);
 
-            const userInfoList = document.querySelector('#userInfo');
+
 
             for(const key in formDataObj){
                 if (formDataObj.hasOwnProperty(key)){
@@ -18,6 +19,13 @@
                     userInfoList.append(li);
                 }
             }
+        } else {
+            const li2 = document.createElement('li');
+            li2.classList.add('list-group-item');
+            li2.textContent = 'No data available';
+            userInfoList.append(li2);
         }
     }
+
+    document.addEventListener('DOMContentLoaded', getLS)
 })()
