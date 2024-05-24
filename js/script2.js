463a9a1a42ae8ac710d275f9ed3873c9f1646fc6
valeriy korovin <1k9o9r5@gmail.com>
2024-05-24 23:15:30 +0300

    HW67_v15

---
+++ js/script2.js
@@ -0,0 +1,15 @@
+"use strict"
+
+(function (){
+
+    const objTodoList = {
+        TODO_ITEMS: 'todo-items',
+        form: document.querySelector('#todoForm'),
+        todoItemContainer: document.querySelector('#todoItems'),
+        currentId: 1,
+        removeAll: document.querySelector('[data-remove-all]'),
+
+        
+    }
+
+})()
