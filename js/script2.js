920b5911010cbdf02b320c5090a536e6e9ea59f1
valeriy korovin <1k9o9r5@gmail.com>
2024-05-25 00:18:06 +0300

    HW67_v16

--- js/script2.js
+++ js/script2.js
@@ -9,7 +9,29 @@
         currentId: 1,
         removeAll: document.querySelector('[data-remove-all]'),
 
-        
+        createTodoItem: (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+
+            const data = {
+                title: null,
+                description: null
+            }
+
+            event.target.querySelectorAll('input, textarea').forEach(input => {
+                data[input.name] = input.value
+            })
+
+            try{
+                const saveItem = saveTodoItem(data);
+                const todoItemHTML
+            }
+        },
+
+        init() {
+            this.form.addEventListener('submit', createTodoItem)
+        }
+
     }
 
 })()
