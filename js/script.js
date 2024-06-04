17f884a60cbdb217389eef3e20dd0bda84f977c4
valeriy korovin <1k9o9r5@gmail.com>
2024-06-04 19:11:03 +0300

    HW69_v2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 "use strict" //eslint-disable-line
+
+const user = {
+    data: {
+        a: 1,
+        b: 2,
+        c: 3,
+        d: {
+            a1: 1,
+            b1: 2,
+            c1: 3,
+            d1: {
+                a2: 3,
+                b2: 3,
+                c2: 3,
+            }
+        },
+    }
+}
+
+const freezeObj = function (obj){
+
+}
