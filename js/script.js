91a14a6e01e29f32eaa89c045cfc2f882a1b4e33
valeriy korovin <1k9o9r5@gmail.com>
2024-06-04 19:06:23 +0300

    HW68_v2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
 "use strict" //eslint-disable-line
+
+const arr = [1, 2, 3, 4];
+
+const ItrObj = (ItrDataStructure) => {
+    const dataArr = Array.from(ItrDataStructure);
+    let currentIndex = 0;
+    return {
+        next(){
+            let done = false;
+
+            if(currentIndex + 1 > dataArr.length){
+                done = true;
+            }
+
+            const result = {
+                value: dataArr[currentIndex],
+                done
+            }
+
+            currentIndex += 1;
+
+            return result
+        },
+    }
+}
+
+const itr2 = ItrObj(arr)
+console.log(itr2);
+
+console.log(itr2.next());
+console.log(itr2.next());
+console.log(itr2.next());
+console.log(itr2.next());
+console.log(itr2.next())
+
+
