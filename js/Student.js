11c3e1ee428131731c0cfa8a86a5a9fc2b532e81
valeriy korovin <1k9o9r5@gmail.com>
2024-06-15 00:24:28 +0300

    HW72_v4

--- js/Student.js
+++ js/Student.js
@@ -1,5 +1,31 @@
 (function (){
-    function Student(){
+    function Student(name, lastname, data){
+        this.name = name;
+        this.lastname = lastname;
+        this.data = data;
+        this.grade = new Array();
+        this.attendance = new Array()
+        this.index = 25;
+
+        Student.prototype.age = function (data){
+
+        };
+
+        Student.prototype.averageMark = function (){
+
+        };
+
+        Student.prototype.present = function (){
+
+        };
+
+        Student.prototype.absent = function (){
+
+        };
+
+        Student.prototype.summary = function (){
+
+        };
 
     }
 
