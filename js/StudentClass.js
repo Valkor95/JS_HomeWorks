2d0e96fb106ce35628d4e961edca360de061aafc
valeriy korovin <1k9o9r5@gmail.com>
2024-06-19 00:06:57 +0300

    HW72_v10

--- js/StudentClass.js
+++ js/StudentClass.js
@@ -1,13 +1,44 @@
 class Student {
-    #firstName: null;
-    #lastName: null;
-    #birthYear: null;
-    #grades: [];
+    #firstName = null;
+    #lastName = null;
+    #birthYear = null;
+    #grades = [];
+    #attendance = [];
+    #currentLessonIndex = 0;
 
-    constructor(firstName, lastName, yearOfBirth) {
+    constructor(firstName, lastName, yearOfBirth, lessonsCount = 25) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.birthYear = yearOfBirth;
+        this.#lessonsCount = 25;
+    }
 
+    setAttendance (bool){
+        if (typeof bool !== 'boolean') throw new Error(`${bool} should be boolean`)
+
+        this.#attendance[this.#currentLessonIndex] = false;
+        this.#currentLessonIndex += 1;
+    }
+
+    present (){
+        this.#attendance[this.#currentLessonIndex] = true;
+        this.#currentLessonIndex += 1;
+    };
+
+    absent (){
+        this.#attendance[this.#currentLessonIndex] = false;
+        this.#currentLessonIndex += 1;
+    };
+
+    summary (){
+
+    }
+
+    setGrade(grade){
+        if(typeof grade !== 'number') throw new Error('Grade is invalid');
+        if(grade > 100 || grade < 0) throw new Error(`Grade cannot be ${grade}`);
+        if(!this.#attendance[this.#currentLessonIndex - 1]) throw new Error(`Student ${this.firstName} is absent`);
+        this.#grades[this.#currentLessonIndex - 1] = grade
     }
 
     get firstName (){
@@ -29,16 +60,27 @@ class Student {
     };
 
     get age() {
-        const currentYear = new Date().getFullYear()
+        const currentYear = new Date().getFullYear();
+        return currentYear - this.#birthYear;
     };
 
-    set birthYear (){
-
+    set birthYear (year){
+        if(typeof year !== 'number' || year.toString().length < 4 || year >= new Date().getFullYear()) throw new Error('Invalid Year');
+        this.#birthYear = year;
     }
 
     get avGrade(){
 
     };
+
+    set #lessonsCount(count){
+       if(typeof count !== 'number' || count < 1) throw new Error('Lessons Count is invalid');
+       this.#grades = new Array(count).fill(null);
+       this.#attendance = new Array(count).fill(null)
+
+    }
 }
 
+const s = new Student('Petr', 'Bulkin', 2000);
+console.log(s)
 export default Student
