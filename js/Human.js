5a5fec19d698d95c075d05fef55b6095372990f7
valeriy korovin <1k9o9r5@gmail.com>
2024-06-22 01:10:35 +0300

    HW74_v2

--- js/Human.js
+++ js/Human.js
@@ -8,7 +8,19 @@ class Human {
 }
 
 class Flat {
+    arrLiver = [];
 
-    constructor() {
+    addLiver(human){
+        if(human instanceof Human){
+            this.arrLiver.push(human);
+        }
+        return this.arrLiver
+
     }
 }
+
+const h = new Human('Val', 'male');
+const f = new Flat();
+f.addLiver(h)
+
+console.log(f.arrLiver)
