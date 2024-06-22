88027caa9260d435cb4bb19ecadace74d96078b5
valeriy korovin <1k9o9r5@gmail.com>
2024-06-22 20:43:40 +0300

    HW75_v3

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 "use strict" //eslint-disable-line
+
+class PostFinder {
+    postContainer = null;
+    searchButton = null;
+    postIdInput = null;
+    constructor() {
+        this.postContainer = document.querySelector('#postContainer');
+        this.searchButton = document.querySelector('#searchPost');
+        this.postIdInput = document.querySelector('#postId');
+
+        this.init();
+    }
+
+    init(){
+        this.onSearchPost = this.onSearchPost.bind(this);
+
+        this.searchButton.addEventListener('click', this.onSearchPost)
+    }
+
+    onSearchPost (){
+        const postId = this.postIdInput.value
+        console.log(postId)
+
+    }
+
+
+}
+
+const p = new PostFinder()
+
+console.log(p)
